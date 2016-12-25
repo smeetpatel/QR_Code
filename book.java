449ca2bfc519fef025d47b4c0067762de9d96095
Shivam Patel <shyam296@ymail.com>
2016-12-26 00:12:28 +0530

    Add files via upload

--- book.java
+++ book.java
@@ -2,10 +2,69 @@
 import java.awt.*;
 import java.applet.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.imageio.ImageIO;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.xml.bind.DatatypeConverter;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
+
+
+class QRCAES {
+	public QRCAES(String data1) throws WriterException, IOException, NotFoundException,Exception {
+		String qrCodeData = data1;
+		System.out.println(qrCodeData);
+		try{
+			SecretKey key=AESEncryption.getSecretEncryptionKey();
+			byte[] cipherText=AESEncryption.encryptText(qrCodeData,key);
+			String filePath = "qrcode.jpg";
+			String charset = "UTF-8";
+		
+			Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
+			hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+		
+			createQRCode(cipherText, filePath, charset, hintMap, 200, 200);
+			
+			System.out.println("QR Code created successfully, Thanks!");	
+			String decryptedText = AESEncryption.decryptText(cipherText,key);
+			System.out.println("Encrypted Text (Binary Form):"+cipherText);
+       			System.out.println("Descrypted Text:"+decryptedText);
+       		
+		}catch(Exception e)
+		{
+			System.out.println("Exception Issued");
+		}
+	}
+
+	public static void createQRCode(byte[] cipherText, String filePath, String charset, Map<EncodeHintType, ErrorCorrectionLevel> hintMap, int qrCodeheight, int qrCodewidth) throws WriterException, IOException {
+		BitMatrix matrix = new MultiFormatWriter().encode( new String(cipherText, charset), BarcodeFormat.QR_CODE, qrCodewidth, qrCodeheight, hintMap);
+		MatrixToImageWriter.writeToFile(matrix, filePath.substring(filePath.lastIndexOf('.') + 1), new File(filePath));
+	}
+}
 
 public class book extends Frame implements ActionListener, ItemListener{
-	String confirmMessage;
+	String confirmMessage,name,age,mobile,email,sex,f_code,arrtime,depttime,to1,from1,cabin1;
 	Button b2=new Button("Cancel");
 	Button b1=new Button("Book Ticket");
 	
@@ -137,19 +196,40 @@ public class book extends Frame implements ActionListener, ItemListener{
 	public void actionPerformed(ActionEvent ae)
 	{
 		
-		String str1 = t1.getText();
-		String str2 = t2.getText();
-		String str3 = t3.getText();
-		String str4 = t4.getText();
-		String str6= from.getSelectedItem();
-		String str7= to.getSelectedItem();
-		String str8= cabin.getSelectedItem();
-		String str9= flight.getSelectedItem();
-		System.out.println(str1+str2+str3+str4+str6+str7+str8+str9);
+		name = t1.getText();
+		age = t2.getText();
+		mobile = t3.getText();
+		email = t4.getText();
+		arrtime= t6.getText();
+		depttime=t5.getText();
+		from1 = from.getSelectedItem();
+		to1 = to.getSelectedItem();
+		cabin1 = cabin.getSelectedItem();
+		f_code = flight.getSelectedItem();
+		
 		if(ae.getActionCommand().equals("Book Ticket"))
  		{
  			confirmMessage="Ticket Booked";
    			setForeground(Color.blue);
+   			
+			if(cbg.getSelectedCheckbox().equals(c2))
+			sex="Male";
+			else	
+			sex="Female";   			
+   			
+   			String data = name+age+sex+f_code+arrtime+depttime;
+   			
+   		
+   			
+   			try{
+   				QRCAES ob1 = new QRCAES(data);
+   				}
+   				catch(Exception e){
+   					System.out.println("here");
+   				}
+   		
+   		
+   		
    		}
    		else if(ae.getActionCommand().equals("Cancel"))
    		{
@@ -159,18 +239,13 @@ public class book extends Frame implements ActionListener, ItemListener{
 	}
 	public void itemStateChanged(ItemEvent ie)
 	{
-		String str5;
+		
 		if(cbg.getSelectedCheckbox().equals(c2))
-			str5="Male";
+			sex="Male";
 		else	
-			
-			str5="Female";
+			sex="Female";
 			
 		
-		
-		System.out.print(str5);
-	
-	
 	}
 	
 	public static void main(String g[])
@@ -189,4 +264,6 @@ class myWindowAdapter extends WindowAdapter
 	  System.exit(0);
 	 }
 }
+
+
 
